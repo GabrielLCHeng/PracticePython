56dbad770f339165e009ada14a7c92b52a92e1cb
GabrielLCH <kiofings@gmail.com>
2020-05-13 10:40:47 +0800

    second commit?

---
+++ 01Character Input.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2020 - age)+100)    # current year
+print(name + " will be 100 years old in the year " + year)
---
+++ 07ListComprehensions.py
@@ -0,0 +1,5 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# find the even number in the list
+print("even: " + str([b for b in a if b%2 == 0]))
+# find the odd number in the list
+print("odd: " + str([b for b in a if b%2 != 0]))
